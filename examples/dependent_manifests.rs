82d475589b52c4288c7de4f481c630cf6bd811cc
Alice Cecile <alice.i.cecil@gmail.com>
2024-04-17 18:01:30 -0400

    Remove the dependent manifests example
    Fixes #1.

--- examples/dependent_manifests.rs
+++
@@ -1,8 +0,0 @@
-//! Data is rarely nicely isolated. Often, you may need to describe how data of one sort (e.g. monsters) relates to data of another sort (e.g. items or levels).
-//!
-//! In this example, a level may contain monsters, items and other game objects.
-//! A monster may in turn reference an item, as part of its loot table.
-//!
-//! By carefully controlling the order in which manifests are processed using system ordering, you can ensure that all the required data is available when you need it.
-
-fn main() {}
